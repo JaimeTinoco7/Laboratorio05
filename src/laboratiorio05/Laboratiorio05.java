c6b251baf34de93d06cd38db7488332b4195cbd5
JaimeTinoco7 <142696839+JaimeTinoco7@users.noreply.github.com>
2023-09-19 23:17:38 -0500

    Add files via upload

---
+++ src/laboratiorio05/Laboratiorio05.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package laboratiorio05;
+
+/**
+ *
+ * @author jaime
+ */
+public class Laboratiorio05 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        FrameListaEstudiante estudiante = new FrameListaEstudiante();
+                estudiante.setLocationRelativeTo(null);
+                estudiante.setVisible(true);
+    }
+    
+}
